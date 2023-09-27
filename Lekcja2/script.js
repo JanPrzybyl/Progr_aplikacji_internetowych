f30a629cf291febfb22f808a990f0c0acd3527a5
Jan <jan.przybyl07@gmail.com>
2023-09-27 22:35:05 +0200

    update

---
+++ Lekcja2/script.js
@@ -0,0 +1,49 @@
+function test(){
+    alert("Hejka naklejka")    
+}
+
+function Dodawanie(){
+    let liczba1 = prompt("Wprowadź liczbe: ")
+    let liczba2 = prompt("Wprowadź liczbe: ")
+
+    let sumaliczb = (liczba1 -+- liczba2)? liczba1 -+- liczba2: liczba1 -+- liczba2;
+    let wynik = (isNaN(sumaliczb))?
+    "Wprowadź tylko liczby":
+    "Suma liczb wynosi "+sumaliczb;
+    alert(wynik)
+}
+
+function Odejmowanie(){
+    let liczba1 = prompt("Wprowadź liczbe: ")
+    let liczba2 = prompt("Wprowadź liczbe: ")
+
+    let sumaliczb = (liczba1 - liczba2)? liczba1 - liczba2: liczba1 - liczba2;
+    let wynik = (isNaN(sumaliczb))?
+    "Wprowadź tylko liczby":
+    "Suma liczb wynosi "+sumaliczb;
+    alert(wynik)
+}
+
+function Mnozenie(){
+    let liczba1 = prompt("Wprowadź liczbe: ")
+    let liczba2 = prompt("Wprowadź liczbe: ")
+
+    let sumaliczb = (liczba1 * liczba2)? liczba1 * liczba2: liczba1 * liczba2;
+    let wynik = (isNaN(sumaliczb))?
+    "Wprowadź tylko liczby":
+    "Suma liczb wynosi "+sumaliczb;
+    alert(wynik)
+}
+
+function Dzielenie(){
+    let liczba1 = prompt("Wprowadź liczbe: ")
+    let liczba2 = prompt("Wprowadź liczbe: ")
+
+    let sumaliczb = (liczba1 / liczba2)? liczba1 / liczba2: liczba1 / liczba2;
+    let wynik = (isNaN(sumaliczb))?
+    "Wprowadź tylko liczby":
+    "Suma liczb wynosi "+sumaliczb;
+    alert(wynik)
+}
+
+
